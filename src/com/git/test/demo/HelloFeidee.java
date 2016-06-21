577ed562dedfccde69282e705e1ef547b8838f50
林世鹏 <Admin@172.21.21.32>
2016-06-21 15:03:29 +0800

    属于测试git的一个demo
    Change-Id: I255531ff6d71ebd087a6ca7d3d1ab9dbfdd50062

---
+++ src/com/git/test/demo/HelloFeidee.java
@@ -0,0 +1,11 @@
+package com.git.test.demo;
+
+public class HelloFeidee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello feidee");
+
+	}
+
+}
